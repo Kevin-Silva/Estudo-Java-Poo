f6938d8bde94ceba70ac51888642815c8fe9423c
kevin-silva <kevintysilva@gmail.com>
2021-03-03 20:17:19 -0300

    Pequena alteração nos codigos!

--- ExecPOO2/src/Application/Program.java
+++ ExecPOO2/src/Application/Program.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import java.util.Locale;
 
 public class Program {
-    public static void main(String args[]){
+    public static void main(String[] args){
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         Employee employee = new Employee();
