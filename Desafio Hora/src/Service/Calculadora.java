d2bf5819edae4be032041f09fb8686dcc85f72ef
Kevin Silva <kevintysilva@gmail.com>
2021-03-30 18:49:52 -0300

    Realizado alteração do codigo, adicionado laços for para checar caso o valor dos minutos seja maior que 60, percorrendo o laço e adicionando a quantidade de horas.

--- Desafio Hora/src/Service/Calculadora.java
+++ Desafio Hora/src/Service/Calculadora.java
@@ -6,7 +6,11 @@ public class Calculadora {
         int somaMinutos = Integer.parseInt(horario[1]) + minutos;
 
         if(somaMinutos >= 60){
-            horario[1] = String.valueOf(somaMinutos - 60);
+            for(int i = somaMinutos; i >= 60; i = i){
+                i -= 60;
+                somaMinutos = i;
+                horario[1] = String.valueOf(somaMinutos);
+            }
             if(Integer.parseInt(horario[1]) < 10){
                 return "0" + horario[1];
             }
@@ -22,8 +26,11 @@ public class Calculadora {
         int somaHorario = Integer.parseInt(horario[1]) + minutos;
 
         if(somaHorario >= 60){
-           int somaHora = Integer.parseInt(horario[0]) + 1;
-            horario[0] = String.valueOf(somaHora);
+            for(int i = somaHorario; i >= 60; i++){
+                i -= 60;
+                int somaHora = Integer.parseInt(horario[0]) + 1;
+                horario[0] = String.valueOf(somaHora);
+            }
 
             if(Integer.parseInt(horario[0]) >= 24){
                 return horario[0] = "00";
